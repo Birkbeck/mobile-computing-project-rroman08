64e8067de502fdbff5ed1957d310641938cbd4c5
Roman Rudolf <roman_rudolf@icloud.com>
2025-05-28 23:09:16 +0200

    created activities for the layouts: create recipe and view recipe

---
+++ app/src/main/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/CreateRecipeActivity.kt
@@ -0,0 +1,25 @@
+package uk.ac.bbk.dcs.mobile_computing_project_rroman08
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import uk.ac.bbk.dcs.mobile_computing_project_rroman08.databinding.ActivityCreateRecipeBinding
+
+class CreateRecipeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateRecipeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCreateRecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
