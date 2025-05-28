525274607db8fc25dbcf3c9fd89366d7242161fd
Roman Rudolf <roman_rudolf@icloud.com>
2025-05-28 22:52:15 +0200

    created an enum for the recipe categories

--- app/src/main/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/RecipeCategory.kt
+++ app/src/main/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/RecipeCategory.kt
@@ -1,4 +1,26 @@
 package uk.ac.bbk.dcs.mobile_computing_project_rroman08
 
 enum class RecipeCategory {
+    BREAKFAST,
+    BRUNCH,
+    LUNCH,
+    DINNER,
+    DESSERT,
+    OTHER;
+
+    override fun toString(): String {
+        // E.g., BREAKFAST displays as Breakfast in the UI
+        // 'it' is the default name for a single parameter in kt lambda expressions
+        // Here it refers to the first character of the string
+        return name.lowercase().replaceFirstChar { it.uppercase() }
+    }
+
+    companion object {
+        // Converts spinner input back to an enum
+        fun fromString(value: String): RecipeCategory {
+            return entries.firstOrNull {
+                it.name.equals(value, ignoreCase = true)
+            } ?: OTHER
+        }
+    }
 }
