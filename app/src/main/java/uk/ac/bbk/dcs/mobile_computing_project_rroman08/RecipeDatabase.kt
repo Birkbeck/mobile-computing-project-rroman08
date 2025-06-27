9ec13690e6c10d90ba8c0da155bb2d6d5835606a
Roman Rudolf <roman_rudolf@icloud.com>
2025-06-27 11:02:36 +0100

    created a room database for recipes

---
+++ app/src/main/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/RecipeDatabase.kt
@@ -0,0 +1,31 @@
+package uk.ac.bbk.dcs.mobile_computing_project_rroman08
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [RecipeEntity::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class RecipeDatabase : RoomDatabase() {
+
+    abstract fun recipeDao(): RecipeDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RecipeDatabase? = null
+        fun getInstance(context: Context): RecipeDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    RecipeDatabase::class.java,
+                    "recipes_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
+}
