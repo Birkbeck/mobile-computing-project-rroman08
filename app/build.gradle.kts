ae05e47bde1263ae8ed2ce58f2af255baa6ca482
Roman Rudolf <roman_rudolf@icloud.com>
2025-06-28 15:33:31 +0100

    re-set to original setting and cleaned whole build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
+    id("kotlin-kapt")
 }
 
 android {
@@ -54,5 +53,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
-//    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
 }
+
--- app/src/main/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/CreateRecipeActivity.kt
+++ app/src/main/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/CreateRecipeActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import uk.ac.bbk.dcs.mobile_computing_project_rroman08.databinding.ActivityCreateRecipeBinding
 
 class CreateRecipeActivity : AppCompatActivity() {
 
