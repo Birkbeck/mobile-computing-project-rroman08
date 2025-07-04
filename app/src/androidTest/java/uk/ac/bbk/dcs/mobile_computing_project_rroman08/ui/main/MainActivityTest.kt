2a7b96dee9706c9e90c5693bd716912b6d8af0f4
Roman Rudolf <roman_rudolf@icloud.com>
2025-07-04 07:41:00 +0100

    test that checks whether recipe card’s button launches correct Activity

--- app/src/androidTest/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/uk/ac/bbk/dcs/mobile_computing_project_rroman08/ui/main/MainActivityTest.kt
@@ -8,12 +8,19 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import uk.ac.bbk.dcs.mobile_computing_project_rroman08.R
 import uk.ac.bbk.dcs.mobile_computing_project_rroman08.ui.edit.EditRecipeActivity
+import uk.ac.bbk.dcs.mobile_computing_project_rroman08.ui.display.DisplayRecipeActivity
+import uk.ac.bbk.dcs.mobile_computing_project_rroman08.data.local.Recipe
+import uk.ac.bbk.dcs.mobile_computing_project_rroman08.data.local.RecipeCategory
+import uk.ac.bbk.dcs.mobile_computing_project_rroman08.data.local.RecipeDatabase
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -38,7 +45,38 @@ class MainActivityTest {
     fun testCreateRecipeButtonLaunchesEditRecipeActivity() {
         ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.button_create_recipe)).perform(click())
-
+        // Check whether launches right Activity
         Intents.intended(hasComponent(EditRecipeActivity::class.java.name))
+    }
+
+    @Test
+    fun testClickingRecipeItemLaunchesDisplayRecipeActivity() = runBlocking {
+        // Requires inserting a recipe into the database before launching activity
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val dao = RecipeDatabase.getInstance(context).recipeDao()
+        val dummyRecipe = Recipe(
+            title = "Monkey Soup",
+            category = RecipeCategory.LUNCH,
+            ingredients = listOf("3 Bananas", "1 Tin of coconut milk", "..."),
+            instructions = listOf("Char bananas on bbq", "Mix bananas with coconut milk in large pot", "...")
+        )
+
+        // Because .insertRecipe() is a suspend fn, it requires to wrap whole test in runBlocking{}
+        val id = dao.insertRecipe(dummyRecipe)
+
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // Wait for RecyclerView to load
+        Thread.sleep(1000)
+
+        // "Click" on the first recipe item
+        onView(
+            allOf(
+                withId(R.id.button_see_recipe),
+                isDescendantOfA(withId(R.id.recycler_view_data))
+            )
+        ).perform(click())
+        // Check whether launches right Activity
+        Intents.intended(hasComponent(DisplayRecipeActivity::class.java.name))
     }
 }
